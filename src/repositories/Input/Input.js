f1d0072f981fcc3a940aefad39eae91cd89c64c9
Lucas <storagemail329@gmail.com>
2024-06-19 15:33:20 -0300

    retirando delete many

--- src/repositories/Input/Input.js
+++ src/repositories/Input/Input.js
@@ -1,9 +1,4 @@
 import Input from "../../models/Input";
-import InputDeleteMany from "./InputDeleteMany";
-import InputSearchDates from "./InputSearchDates";
-import InputSearchFloats from "./InputSearchFloats";
-import InputSearchIntegers from "./InputSearchIntegers";
-import InputSearchSimpleStrings from "./InputSearchSimpleStrings";
 
 class InputsList {
   async List() {
@@ -75,58 +70,6 @@ class InputsList {
     } catch (e) {
       // mudar o retorno para null ao terminar
       return console.log(e);
-    }
-  }
-
-  async DeleteMany(where, data) {
-    switch(where) {
-      case 'name':
-        const findByName = InputSearchSimpleStrings.SearchByName(data);
-        if(!findByName) return null;
-        await InputDeleteMany.DeleteMany(where, data);
-        return `Insumos com ${where}: ${data} deletados`
-
-      case 'type':
-        const findByType = InputSearchSimpleStrings.SearchByType(data);
-        if(!findByType) return null;
-        await InputDeleteMany.DeleteMany(where, data);
-        return `Insumos com ${where}: ${data} deletados`
-
-      case 'quantity':
-        const findByQuantity = InputSearchIntegers.SearchByQuantity(data);
-        if(!findByQuantity) return null;
-        await InputDeleteMany.DeleteMany(where, data);
-        return `Insumos com ${where}: ${data} deletados`
-
-      case 'totalWeight':
-        const findByTotalWeight = InputSearchFloats.SearchByTotalWeight(data);
-        if(!findByTotalWeight) return null;
-        await InputDeleteMany.DeleteMany(where, data);
-        return `Insumos com ${where}: ${data} deletados`
-
-      case 'weightperunit':
-        const findByWeightPerUnit = InputSearchFloats.SearchByWeightPerUnit(data);
-        if(!findByWeightPerUnit) return null;
-        await InputDeleteMany.DeleteMany(where, data);
-        return `Insumos com ${where}: ${data} deletados`
-
-      case 'supplier':
-        const findBySupplier = InputSearchSimpleStrings.SearchBySupplier(data);
-        if(!findBySupplier) return null;
-        await InputDeleteMany.DeleteMany(where, data);
-        return `Insumos com ${where}: ${data} deletados`
-
-      case 'expirationDate':
-        const findByExpirationDate = InputSearchDates.SearchByExpirationDate(data);
-        if(!findByExpirationDate) return null;
-        await InputDeleteMany.DeleteMany(where, data);
-        return `Insumos com ${where}: ${data} deletados`
-
-      case 'entrydate':
-        const findByEntryDate = InputSearchDates.SearchByEntryDate(data);
-        if(!findByEntryDate) return null;
-        await InputDeleteMany.DeleteMany(where, data);
-        return `Insumos com ${where}: ${data} deletados`
     }
   }
 
