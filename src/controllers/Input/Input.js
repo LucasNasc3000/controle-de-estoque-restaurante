e721da9fb260d36bb44773a91472442db6d5f55d
lucasNasc <lucasfortunato328@gmail.com>
2024-12-27 16:05:26 -0300

    logica para restricao em atualizacoes de insumos corrigida

--- src/controllers/Input/Input.js
+++ src/controllers/Input/Input.js
@@ -1,10 +1,10 @@
 /* eslint-disable camelcase */
 /* eslint-disable consistent-return */
 import { BadRequest } from '../../errors/clientErrors';
-import { Forbidden } from '../../errors/forbidden';
 import { InternalServerError } from '../../errors/serverErrors';
 import Validation from '../../middlewares/fieldValidations/Validation';
 import InputMethods from '../../repositories/Input/Input';
+import InputSearchIntegers from '../../repositories/Input/InputSearchIntegers';
 
 class InputController {
   async Store(req, res, next) {
@@ -41,9 +41,10 @@ class InputController {
   async Update(req, res, next) {
     try {
       const { id } = req.params;
-      const { employee_id } = req.body;
 
-      if (employee_id) throw new Forbidden('Ação não permitida');
+      const inputSearch = await InputSearchIntegers.SearchByID(id);
+
+      if (!inputSearch) throw new BadRequest('Insumo não registrado');
 
       const validations = Validation.MainValidations(req.body, false, false, false, true);
       const inputValidations = Validation.InputsValidation(req.body);
@@ -51,11 +52,14 @@ class InputController {
       if (validations !== null) throw new BadRequest(validations);
       if (inputValidations !== null) throw new BadRequest(inputValidations);
 
+      const { employee_id, ...rest } = req.body;
+
       // Funciona sem await mas não retorna os dados na requisição caso ela seja feita com um app de
       // requisições como insomnia.
-      const inputUpdate = await InputMethods.Update(id, req.body);
+      const inputUpdate = await InputMethods.Update(id, rest);
 
-      if (inputUpdate === 'insumo não encontrado') throw new BadRequest('Insumo não registrado');
+      if (inputUpdate === 'insumo não encontrado') throw new BadRequest('Insumo não encontrado');
+
       if (!inputUpdate) throw new InternalServerError('Erro interno');
 
       return res.status(200).send(inputUpdate);
