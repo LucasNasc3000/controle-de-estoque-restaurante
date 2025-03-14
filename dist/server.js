28ffff4b7126c7628d0a227e29f7f16ca9cfe4e8
lucasNasc <lucasfortunato328@gmail.com>
2025-03-14 13:24:57 -0300

    pequenas correcoes

--- dist/server.js
+++ dist/server.js
@@ -1,6 +1,8 @@
-"use strict"; function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }var _app = require('./app'); var _app2 = _interopRequireDefault(_app);
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; } const _app = require('./app');
+
+const _app2 = _interopRequireDefault(_app);
 require('./database');
 
-_app2.default.listen(3000);
+_app2.default.listen(3333);
 
-console.log('Acesso na porta http://localhost:3000');
+console.log('Acesso na porta http://localhost:3333');
