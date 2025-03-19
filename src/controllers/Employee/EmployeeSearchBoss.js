332f1635b16a78475cca7128a0aa2d96314b7501
lucasNasc <lucasfortunato328@gmail.com>
2025-03-19 17:49:41 -0300

    correcoes logicas

--- src/controllers/Employee/EmployeeSearchBoss.js
+++ src/controllers/Employee/EmployeeSearchBoss.js
@@ -1,6 +1,5 @@
 /* eslint-disable consistent-return */
 import { Forbidden } from '../../errors/forbidden';
-import { NotFound } from '../../errors/notFound';
 import { InternalServerError } from '../../errors/serverErrors';
 import EmployeeSearchBoss from '../../repositories/Employee/EmployeeSearchBoss';
 import EmployeesSearchCredentials from '../../repositories/Employee/EmployeeSearchCredentials';
@@ -24,7 +23,9 @@ class EmployeesSearchBossController {
 
       if (!employeeBossFinder) throw new InternalServerError('Erro interno');
 
-      if (employeeBossFinder.length < 1) throw new NotFound('Chefe não encontrado');
+      if (employeeBossFinder.length < 1) {
+        return res.status(204).send('Sem registros');
+      }
 
       return res.status(200).json(employeeBossFinder);
     } catch (err) {
@@ -39,7 +40,7 @@ class EmployeesSearchBossController {
 
       const findByEmail = await EmployeesSearchCredentials.SearchByEmail(email);
 
-      if (findByEmail.dataValues.boss !== boss) throw new Forbidden('Ação não autorizada');
+      if (findByEmail.dataValues.boss !== boss && findByEmail.dataValues.boss !== null) throw new Forbidden('Ação não autorizada');
 
       const employeeBossFinder = await EmployeeSearchBoss.SearchByBossForListItems(boss);
 
